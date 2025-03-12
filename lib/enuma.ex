cef22fb1b1396b072ea22c45d8447eaf3327f23e
Alisina Bahadori <alisina.bm@gmail.com>
2025-03-12 16:20:32 -0400

    Initial Commit

---
+++ lib/enuma.ex
@@ -0,0 +1,18 @@
+defmodule Enuma do
+  @moduledoc """
+  Documentation for `Enuma`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Enuma.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/enuma_test.exs
@@ -0,0 +1,8 @@
+defmodule EnumaTest do
+  use ExUnit.Case
+  doctest Enuma
+
+  test "greets the world" do
+    assert Enuma.hello() == :world
+  end
+end
