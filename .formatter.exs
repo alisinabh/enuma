cd14d6118ba0608cfd06ee49744ade88666b8ba7
Alisina Bahadori <alisina.bm@gmail.com>
2025-03-17 23:56:20 -0400

    Add locals_without_parens export

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,11 @@
 # Used by "mix format"
+
+locals_without_parens = [defenum: 1, item: :*]
+
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: [defenum: 1, item: :*]
+  locals_without_parens: locals_without_parens,
+  export: [
+    locals_without_parens: locals_without_parens
+  ]
 ]
